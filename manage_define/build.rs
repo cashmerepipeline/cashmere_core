f849637c2a0211c1be027a632cfde2b45530be95
yes7rose <yes7rose@sina.com>
2021-11-15 22:42:06 +0800

    修改属性id生成方式

--- manage_define/build.rs
+++ manage_define/build.rs
@@ -15,9 +15,6 @@ use toml::map::Map;
 
 use property_field::*;
 
-// use define_utils::{get_toml_map, get_name, get_id, get_schema};
-
-
 /// 读取文件为toml_map
 pub fn get_toml_map(toml_path: &String) -> Option<toml::map::Map<String, toml::Value>> {
     let mut toml_file =
@@ -75,30 +72,36 @@ pub fn get_name(toml_map: &toml::map::Map<String, toml::Value>) -> Option<Docume
     }
 }
 
-/// 管理描写
+/**
+ * 取得管理数据类型
+ * @param toml_map 定义toml数据
+ * @return 管理字段列表bson
+ */
 pub fn get_schema(toml_map: &toml::map::Map<String, toml::Value>) -> Option<Bson> {
-    let id = get_id(toml_map).unwrap();
+    let manage_id = get_id(toml_map).unwrap();
     let value = toml_map.get("schema").expect("取得描写数据失败");
+
     let mut schema_vec: Vec<Document> = Vec::new();
-    for v in value.as_array().unwrap().iter() {
+    for (field_id, v) in value.as_array().unwrap().iter().enumerate() {
         let field_toml = match v.as_table() {
             None => {
-                println!("错误 {}-{}", id, v.to_string());
+                println!("错误 {}-{}", manage_id, v.to_string());
                 panic!("定义文件错误")
             }
             Some(r) => r
         };
-        let field: PropertyField = PropertyField::from_toml(field_toml);
+
+        let field: PropertyField = PropertyField::from_toml(field_toml, &(field_id as i32));
         // println!("{:?}", field);
+
         let mut temp_doc = Document::new();
-        temp_doc.insert("id", field.id);
+        temp_doc.insert("id", (field_id + 1 + 2000) as i32);
         temp_doc.insert("data_type", field.data_type.to_string());
         temp_doc.insert("name", bson::to_document(&field.name).unwrap());
         temp_doc.insert("removed", &field.removed);
 
         schema_vec.push(temp_doc);
     }
-    // toml::from_str(&value.to_string()).expect("转换描写列表失败");
 
     match bson::to_bson(&schema_vec) {
         Ok(r) => Some(r),
--- property_field/src/lib.rs
+++ property_field/src/lib.rs
@@ -6,16 +6,17 @@ Created:  2020-11-26T12:47:25.692Z
 Modified: !date!
 */
 
-use std::{fmt::{Display, Formatter}};
+use std::fmt::{Display, Formatter};
 
 use linked_hash_map::LinkedHashMap;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 use bson;
 use toml;
 
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
 pub enum FieldDataType {
+    None,
     Bool,
     Int32,
     Int64,
@@ -23,13 +24,13 @@ pub enum FieldDataType {
     UInt64,
     F32,
     F64,
+    Range,
     String,
     List,
     Bytes,
     Map,
     Date,
     DateTime,
-    None,
 }
 
 #[derive(Debug, Serialize, Deserialize, Hash, Eq, PartialEq)]
@@ -42,6 +43,13 @@ pub enum Language {
 pub struct Name {
     language: Language,
     name: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Range<T> {
+    min: T,
+    max: T,
+    value: T,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
@@ -62,6 +70,7 @@ impl Display for FieldDataType {
             FieldDataType::UInt64 => write!(f, "UInt64"),
             FieldDataType::F32 => write!(f, "F32"),
             FieldDataType::F64 => write!(f, "F64"),
+            FieldDataType::Range => write!(f, "Range"),
             FieldDataType::String => write!(f, "String"),
             FieldDataType::List => write!(f, "List"),
             FieldDataType::Bytes => write!(f, "Bytes"),
@@ -75,10 +84,39 @@ impl Display for FieldDataType {
 
 impl From<String> for FieldDataType {
     fn from(s: String) -> Self {
-        if s == "Bool".to_string() { return FieldDataType::Bool; } else if s == "Int32".to_string() { return FieldDataType::Bool; } else if s == "Int64".to_string() { return FieldDataType::Bool; } else if s == "UInt32".to_string() { return FieldDataType::Bool; } else if s == "UInt64".to_string() { return FieldDataType::Bool; } else if s == "F32".to_string() { return FieldDataType::Bool; } else if s == "F64".to_string() { return FieldDataType::Bool; } else if s == "String".to_string() { return FieldDataType::Bool; } else if s == "List".to_string() { return FieldDataType::Bool; } else if s == "Bytes".to_string() { return FieldDataType::Bool; } else if s == "Map".to_string() { return FieldDataType::Bool; } else if s == "Date".to_string() { return FieldDataType::Bool; } else if s == "DateTime".to_string() { return FieldDataType::Bool; } else { return FieldDataType::None; }
+        if s == "Bool".to_string() {
+            return FieldDataType::Bool;
+        } else if s == "Int32".to_string() {
+            return FieldDataType::Int32;
+        } else if s == "Int64".to_string() {
+            return FieldDataType::Int64;
+        } else if s == "UInt32".to_string() {
+            return FieldDataType::UInt32;
+        } else if s == "UInt64".to_string() {
+            return FieldDataType::UInt64;
+        } else if s == "F32".to_string() {
+            return FieldDataType::F32;
+        } else if s == "F64".to_string() {
+            return FieldDataType::F64;
+        } else if s == "Range".to_string() {
+            return FieldDataType::Range;
+        } else if s == "String".to_string() {
+            return FieldDataType::String;
+        } else if s == "List".to_string() {
+            return FieldDataType::List;
+        } else if s == "Bytes".to_string() {
+            return FieldDataType::Bytes;
+        } else if s == "Map".to_string() {
+            return FieldDataType::Bytes;
+        } else if s == "Date".to_string() {
+            return FieldDataType::Date;
+        } else if s == "DateTime".to_string() {
+            return FieldDataType::DateTime;
+        } else {
+            return FieldDataType::None;
+        }
     }
 }
-
 
 impl Display for Language {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
@@ -91,23 +129,21 @@ impl Display for Language {
 
 impl PropertyField {
     pub fn has_name(&self, name: &String) -> bool {
-        let names: Vec<String> =
-            self.name.values()
-                .map(|v| v.clone())
-                .collect();
+        let names: Vec<String> = self.name.values().map(|v| v.clone()).collect();
 
         names.contains(name)
     }
 
-    pub fn from_toml(toml: &toml::map::Map<String, toml::Value>) -> PropertyField {
-        let id = toml.get("id").unwrap().as_integer().unwrap() as i32;
-        let name: LinkedHashMap<String, String> = toml::from_str(&toml.get("name").unwrap().to_string()).unwrap();
+    pub fn from_toml(toml: &toml::map::Map<String, toml::Value>, id:&i32) -> PropertyField {
+        let name: LinkedHashMap<String, String> =
+            toml::from_str(&toml.get("name").unwrap().to_string()).unwrap();
         // println!("{:?}", name);
-        let data_type: FieldDataType = toml::from_str(&toml.get("data_type").unwrap().to_string()).unwrap();
+        let data_type: FieldDataType =
+            toml::from_str(&toml.get("data_type").unwrap().to_string()).unwrap();
         let removed: bool = toml.get("removed").unwrap().as_bool().unwrap();
 
         PropertyField {
-            id: id,
+            id: *id,
             name: name,
             data_type: data_type,
             removed: removed,
@@ -116,9 +152,11 @@ impl PropertyField {
 
     pub fn from_bson(doc: &bson::Document) -> PropertyField {
         let id = doc.get_i32("id").unwrap();
-        let name: LinkedHashMap<String, String> = bson::from_document(doc.get_document("name").unwrap().clone()).unwrap();
+        let name: LinkedHashMap<String, String> =
+            bson::from_document(doc.get_document("name").unwrap().clone()).unwrap();
         // println!("{:?}", name);
-        let data_type: FieldDataType = bson::from_bson(doc.get("data_type").unwrap().clone()).unwrap();
+        let data_type: FieldDataType =
+            bson::from_bson(doc.get("data_type").unwrap().clone()).unwrap();
         let removed: bool = doc.get_bool("removed").unwrap();
 
         PropertyField {
