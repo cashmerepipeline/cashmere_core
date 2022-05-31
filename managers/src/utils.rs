58c651baa7b251898e5208f12e43f5fba6ab0072
yes7rose <yes7rose@sina.com>
2022-05-31 22:44:11 +0800

    修正id错误

--- managers/src/utils.rs
+++ managers/src/utils.rs
@@ -6,10 +6,10 @@ use crate::{Manager, ManagerTrait};
 
 /// 新建一个实体记录
 pub async fn make_new_entity_document(manager: &Arc<Manager>) -> Option<Document> {
-    if let new_id = manager.get_new_entity_id().await {
+    if let new_id = manager.get_new_entity_id().await.unwrap() {
         let mut new_entity_doc = Document::new();
-        new_entity_doc.insert("_id", new_id.clone());
-        new_entity_doc.insert(ID_FIELD_ID.to_string(), new_id.clone());
+        new_entity_doc.insert("_id", new_id.to_string());
+        new_entity_doc.insert(ID_FIELD_ID.to_string(), new_id.to_string());
 
         Some(new_entity_doc)
     } else {
