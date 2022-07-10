ded16ff2adbf6b157c6fd82f3524ba1f23587331
闫刚 <yangang.richerd@bytedance.com>
2022-07-10 12:37:31 +0800

    升级parking-lot

--- auth/src/test.rs
+++ auth/src/test.rs
@@ -8,9 +8,9 @@ mod tests {
         use crate::jwt::hash_password;
 
         let _root_passwd =
-            futures::executor::block_on(hash_password::hash_password(&"root".to_string())).unwrap();
+            futures::executor::block_on(hash_password(&"root".to_string())).unwrap();
         let _user_passwd =
-            futures::executor::block_on(hash_password::hash_password(&"user".to_string())).unwrap();
+            futures::executor::block_on(hash_password(&"user".to_string())).unwrap();
         // println!("{}", root_passwd);
         // println!("{}", user_passwd);
         // assert_eq!("$argon2i$v=19$m=4096,t=3,p=1$MjAyMC0xMS0yNlQxNjoyODoxNS40NzE2MzcrMDA6MDA$JuVkdGb42sZRWEVer/mDWx9EpUmfLf7pwOp2Aw4zas4".as_bytes(), root_passwd.as_bytes());
