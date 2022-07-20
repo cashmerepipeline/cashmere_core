59dfed48f2690c53d145587c17f3428c8f6be488
yangang.richerd <yangang.richerd@bytedance.com>
2022-07-21 00:21:19 +0800

    修整设置

--- view/src/enum_filter_rule.rs
+++ view/src/enum_filter_rule.rs
@@ -3,13 +3,15 @@ use serde::{Deserialize, Serialize};
 /// 过滤规则
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum FilterRule {
+    // 无限制
     NoLimit,
-    // 主 所有
-    OwnerALl,
-    // 组 所有
-    GroupALl,
+
     //只有组的
     OnlyGroup,
+
     // 只有主的
     OnlyOwner,
+
+    // 系统使用
+    OnlySystem,
 }
